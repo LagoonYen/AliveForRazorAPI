53a13c2a9a909a24335f9e90694e58857b14d087
ElevenHsiao <nicky17558@hotmail.com>
2022-01-28 14:57:41 +0800

    update

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -5,8 +5,20 @@ namespace AliveStoreTemplate.Pages
 {
     public class LoginModel : PageModel
     {
+
+        [BindProperty]
+        public string Account { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
         public void OnGet()
         {
+            var myAccount = Account;
+        }
+
+        public void OnPostMyLogin()
+        {
+            var myAccount = Account;
         }
     }
 }
